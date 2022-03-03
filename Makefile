695ae295cbc924a59040f81aead6f2b9420077de
Andrew Pair <awpair03@gmail.com>
2022-03-02 19:56:03 -0500

    Moved into graphical text mode instead of BIOS

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
-export target=i686 # wowzers
-
 all: build/bootloader.bin build/kernel.bin 
 
 build/kernel.bin: build/stage2.o build/kernel.o
-	/home/andrew/opt/cross/bin/$(target)-elf-ld -o build/kernel.tmp -Ttext 0x7e00 build/stage2.o build/kernel.o
+	/home/andrew/opt/cross/bin/i686-elf-ld -o build/kernel.tmp -Ttext 0x7e00 build/stage2.o build/kernel.o
 	objcopy -O binary build/kernel.tmp build/kernel.bin
 
 build/bootloader.bin: src/bootloader.asm
@@ -13,7 +11,7 @@ build/stage2.o: src/stage2.asm
 	nasm src/stage2.asm -f elf -o build/stage2.o
 
 build/kernel.o: src/kernel.c
-	/home/andrew/opt/cross/bin/$(target)-elf-gcc -ffreestanding -m32 -c "src/kernel.c" -o "build/kernel.o"
+	/home/andrew/opt/cross/bin/i686-elf-gcc -ffreestanding -m32 -c "src/kernel.c" -o "build/kernel.o"
 
 clean:
 	rm -rf build/*
--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,49 +1,71 @@
 [ORG 0x7c00]
 
-xor ax, ax  ; AX = 0
-mov ds, ax  ; DS = AX
+xor ax, ax
+mov ds, ax
+mov ss, ax
+mov sp, 0x9c00
 
-mov ax, 0xABCD
+cld
 
-mov word [reg16], ax
-call putreg16
+mov ax, 0xb800
+mov es, ax
+
+mov si, msg
+call sprint
 
 hang:
-    jmp hang
+        jmp hang
 
-puts:       lodsb
-            mov ah, 0x0E    ;Bios print mode
-            mov bh, 0
-            mov bl, 4
-            mov cx, 1
+dochar: call cprint
+sprint: lodsb
+        cmp al, 0
+        jne dochar
+        add byte [ypos], 1
+        add byte [xpos], 0
+        ret
+cprint: mov ah, 0x0F
+        mov cx, ax
+        movzx ax, byte [ypos]
+        mov dx, 160
+        mul dx
+        movzx bx, byte [xpos]
+        shl bx, 1
+        mov di, 0
+        add di, ax
+        add di, bx
+        mov ax, cx
 
-            or al, al
-            jz loop_exit
-            int 0x10
-            jmp puts
-            loop_exit:ret            
+        stosw
+        add byte [xpos], 1
 
-putreg16:   mov di, outstr16
-            mov ax, [reg16]
-            mov si, hexstr
-            mov cx, 4
-hexloop:    rol ax, 4
-            mov bx, ax
-            and bx, 0x000f
-            mov bl, [si + bx]
-            mov [di+2], bl
-            inc di
-            dec cx
-            jnz hexloop
+        ret
 
-            mov si, outstr16
-            call puts
+printreg16: 
+        mov di, outstr16
+        mov ax, [reg16]
+        mov si, hexstr
+        mov cx, 4
+hexloop:
+        rol ax, 4
+        mov bx, ax
+        and bx, 0x0f
+        mov bl, [si + bx]
+        mov [di], bl
+        inc di
+        dec cx
+        jnz hexloop
+        mov si, outstr16
+        call sprint
+        ret
 
-hexstr db '0123456789ABCDEF'
-outstr16 db '0x0000', 0
-reg16 dw 0
+ypos        db 0
+xpos        db 0
 
-msg db "BURG v0.0.1"
+hexstr      db '0123456789ABCDEF'
+outstr16    db '0x0000', 0
+reg16       dw 0
+
+msg         db "Nierix-Load v0.0.1"
 times 510-($-$$) db 0
 db 0x55
 db 0xaa
