512e262435f7cc32941156785e189e770809c35b
Andrew Pair <awpair03@gmail.com>
2022-03-04 12:09:15 -0500

    Moved print functions to asm includes subfolder

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -16,47 +16,7 @@ call sprint
 hang:
         jmp hang
 
-dochar: call cprint
-sprint: lodsb
-        cmp al, 0
-        jne dochar
-        add byte [ypos], 1
-        add byte [xpos], 0
-        ret
-cprint: mov ah, 0x0F
-        mov cx, ax
-        movzx ax, byte [ypos]
-        mov dx, 160
-        mul dx
-        movzx bx, byte [xpos]
-        shl bx, 1
-        mov di, 0
-        add di, ax
-        add di, bx
-        mov ax, cx
-
-        stosw
-        add byte [xpos], 1
-
-        ret
-
-printreg16: 
-        mov di, outstr16
-        mov ax, [reg16]
-        mov si, hexstr
-        mov cx, 4
-hexloop:
-        rol ax, 4
-        mov bx, ax
-        and bx, 0x0f
-        mov bl, [si + bx]
-        mov [di], bl
-        inc di
-        dec cx
-        jnz hexloop
-        mov si, outstr16
-        call sprint
-        ret
+%include "src/asm/print.asm"
 
 ypos        db 0
 xpos        db 0
@@ -65,7 +25,7 @@ hexstr      db '0123456789ABCDEF'
 outstr16    db '0x0000', 0
 reg16       dw 0
 
-msg         db "Nierix-Load v0.0.1"
+msg         db "BOOTPROC"
 times 510-($-$$) db 0
 db 0x55
 db 0xaa
